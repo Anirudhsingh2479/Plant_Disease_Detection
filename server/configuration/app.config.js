c277f0f4ca6def4659a0447ff4d4762a5ed4f9df
Anirudhsingh2479 <166243648+Anirudhsingh2479@users.noreply.github.com>
2026-05-18 17:31:02 +0530

    added the server

---
+++ server/configuration/app.config.js
@@ -0,0 +1,12 @@
+const getEnv = require("../utils/get-env").getEnv;
+
+const appConfig = () => ({
+  NODE_ENV: getEnv("NODE_ENV", "development"),
+  PORT: getEnv("PORT", "5000"),
+  BASE_PATH: getEnv("BASE_PATH", "/api"),
+//   MONGO_URI: getEnv("MONGO_URI", ""),
+});
+
+module.exports = {
+  config: appConfig(),
+};
