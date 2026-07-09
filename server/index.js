b28189207b669b6e6cc761e58ba95b26cdf743a6
Raj Aryan <rmishra+flexera@flexera.com>
2026-07-09 12:59:22 +0530

    fixing cors

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://plant-disease-detection-peach.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:5174", process.env.FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie", "x-request-source"],
